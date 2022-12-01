0c6a5e342d240520565d93f9a5c20925bd563dcf
17612937572 <2480243521@qq.com>
2022-12-01 18:31:26 +0800

    update: 格式化序列化时间

--- project_drf/activity/serializers.py
+++ project_drf/activity/serializers.py
@@ -3,6 +3,11 @@ from .models import Activity
 
 # 创建序列化器，在view中会被调用
 class ActivityModelSerializer(serializers.ModelSerializer):
+
+    create_time = serializers.DateTimeField(label='报名时间', required=False, format='%Y-%d-%m %H:%M:%S')
+    start_date = serializers.DateField(label='开始日期', required=True, format='%Y-%d-%m')
+    start_time = serializers.TimeField(label='开始时间', required=True, format='%H:%M:%S')
+
     class Meta:
         # models 指明该序列化器处理的字段从模型类Activity参考生成
         # fields 指明该序列化器包含模型类中的哪些字段，'all'代表包含所有字段
