f6a49b5a8f0eac0f7ebe0602a1f4c29abbaeef2d
17612937572 <2480243521@qq.com>
2022-12-27 10:24:00 +0800

    update: apply form

--- project_react/src/pages/ActivityTableList/components/ApplyForm.tsx
+++ project_react/src/pages/ActivityTableList/components/ApplyForm.tsx
@@ -1,11 +1,8 @@
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import {
-  ProForm,
-  ProFormText,
-} from '@ant-design/pro-components';
-// import { useIntl } from '@umijs/max';
+import { ProForm, ProFormText } from '@ant-design/pro-components';
+import { useIntl } from '@umijs/max';
 import React from 'react';
-
+import { Modal } from 'antd';
 
 export type FormValueType = {
   target?: string;
@@ -23,52 +20,68 @@ export type ApplyFormProps = {
 };
 
 const ApplyForm: React.FC<ApplyFormProps> = (props) => {
+  const intl = useIntl();
   return (
-    <ProForm>
-      <ProFormText
-        name="name"
-        fieldProps={{
-          size: 'large',
-          prefix: <UserOutlined className={'prefixIcon'} />,
+    <>
+      <Modal
+        width={640}
+        bodyStyle={{ padding: '32px 40px 48px' }}
+        destroyOnClose
+        title={intl.formatMessage({
+          id: 'pages.searchTable.applyForm.title',
+          defaultMessage: '填写报名信息',
+        })}
+        visible={props.applyModalVisible}
+        onCancel={() => {
+          props.onCancel();
         }}
-        placeholder={'姓名: admin or user'}
-        rules={[
-          {
-            required: true,
-            message: '请输入姓名!',
-          },
-        ]}
-      />
-      <ProFormText
-        name="sex"
-        fieldProps={{
-          size: 'large',
-          prefix: <LockOutlined className={'prefixIcon'} />,
-        }}
-        placeholder={'性别: ant.design'}
-        rules={[
-          {
-            required: true,
-            message: '请输入密码！',
-          },
-        ]}
-      />
-      <ProFormText
-        name="age"
-        fieldProps={{
-          size: 'large',
-          prefix: <LockOutlined className={'prefixIcon'} />,
-        }}
-        placeholder={'年龄: ant.design'}
-        rules={[
-          {
-            required: true,
-            message: '请输入年龄！',
-          },
-        ]}
-      />
-    
-</ProForm>
+      >
+        <ProForm>
+          <ProFormText
+            name="name"
+            fieldProps={{
+              size: 'large',
+              prefix: <UserOutlined className={'prefixIcon'} />,
+            }}
+            placeholder={'姓名: admin or user'}
+            rules={[
+              {
+                required: true,
+                message: '请输入姓名!',
+              },
+            ]}
+          />
+          <ProFormText
+            name="sex"
+            fieldProps={{
+              size: 'large',
+              prefix: <LockOutlined className={'prefixIcon'} />,
+            }}
+            placeholder={'性别: ant.design'}
+            rules={[
+              {
+                required: true,
+                message: '请输入密码！',
+              },
+            ]}
+          />
+          <ProFormText
+            name="age"
+            fieldProps={{
+              size: 'large',
+              prefix: <LockOutlined className={'prefixIcon'} />,
+            }}
+            placeholder={'年龄: ant.design'}
+            rules={[
+              {
+                required: true,
+                message: '请输入年龄！',
+              },
+            ]}
+          />
+        </ProForm>
+      </Modal>
+    </>
   );
 };
 
