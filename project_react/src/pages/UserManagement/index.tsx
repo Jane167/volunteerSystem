b9c7d906829f6804ed20cfc7fa3cc5d92ede4074
17612937572 <2480243521@qq.com>
2023-01-12 18:11:21 +0800

    update: user-management

--- project_react/src/pages/UserManagement/index.tsx
+++ project_react/src/pages/UserManagement/index.tsx
@@ -1,113 +1,167 @@
 import {
-  ColumnHeightOutlined,
+  CheckCircleOutlined,
+  CloseCircleOutlined,
+  InfoCircleOutlined,
+  ManOutlined,
   PlusOutlined,
-  SearchOutlined,
-  SettingOutlined,
+  QuestionCircleOutlined,
+  WomanOutlined,
 } from '@ant-design/icons';
-import { PageContainer } from '@ant-design/pro-components';
-import { Card, Table, Tag, Space, Button, Tooltip } from 'antd';
-const { Column } = Table;
+import {
+  PageContainer,
+  ProColumns,
+  ProDescriptions,
+  ProDescriptionsItemProps,
+} from '@ant-design/pro-components';
+import { ProTable } from '@ant-design/pro-components';
+import { Button, Drawer, Tag } from 'antd';
+import React, { useState } from 'react';
 
-import React from 'react';
-const dataSource = [
+export type UserTableListItem = {
+  key: number;
+  id: number;
+  username: string;
+  password: string;
+  role: number;
+  create_time: string;
+};
+
+const userTableListDataSource: UserTableListItem[] = [
   {
-    key: '1',
-    id: '1',
-    username: 'admin',
+    key: 1,
+    id: 1,
+    username: '李佳音',
     password: '123',
-    role: '普通用户',
-    createTime: '2022-1-12 14:00:00',
+    role: 1,
+    create_time: '2023-1-12 16:00:00',
   },
   {
-    key: '1',
-    id: '1',
-    username: 'admin',
+    key: 1,
+    id: 1,
+    username: '李佳音',
     password: '123',
-    role: '普通用户',
-    createTime: '2022-1-12 14:00:00',
+    role: 2,
+    create_time: '2023-1-12 16:00:00',
   },
   {
-    key: '1',
-    id: '1',
-    username: 'admin',
+    key: 1,
+    id: 1,
+    username: '李佳音',
     password: '123',
-    role: '公益企业',
-    createTime: '2022-1-12 14:00:00',
-  },
-  {
-    key: '1',
-    id: '1',
-    username: 'admin',
-    password: '123',
-    role: '管理员',
-    createTime: '2022-1-12 14:00:00',
+    role: 3,
+    create_time: '2023-1-12 16:00:00',
   },
 ];
+const ApplyTableList: React.FC = () => {
+  const [showDetail, setShowDetail] = useState<boolean>(false);
+  const [currentRow, setCurrentRow] = useState<UserTableListItem>();
+  const columns: ProColumns<UserTableListItem>[] = [
+    {
+      title: 'Id',
+      width: 80,
+      dataIndex: 'id',
+      search: false,
+      sorter: (a, b) => a.id - b.id,
+    },
+    {
+      title: '用户名',
+      dataIndex: 'username',
+      align: 'center',
+      search: false,
+    },
+    {
+      title: '密码',
+      dataIndex: 'password',
+      search: false,
+    },
+    {
+      title: '角色',
+      dataIndex: 'role',
+      render: (role) => {
+        console.log(role, 'rocord');
+        let color;
+        let text = '';
+        if (role === 1) {
+          color = 'warning';
+          text = '管理员';
+        } else if (role === 2) {
+          color = 'processing';
+          text = '公益企业';
+        } else if (role === 3) {
+          color = 'success';
+          text = '普通用户';
+        }
+        return <Tag color={color}>{text}</Tag>;
 
-const UserManagement: React.FC = () => {
+      },
+    },
+  
+    {
+      title: '注册时间',
+      dataIndex: 'create_time',
+      search: false,
+    },
+    {
+      title: '操作',
+      width: 300,
+      key: 'option',
+      valueType: 'option',
+      render: (dom, entity) => [
+        <a
+          onClick={() => {
+            setCurrentRow(entity);
+            setShowDetail(true);
+          }}
+        >
+          查看详情
+        </a>,
+        <a key="link">重置密码</a>,
+        <a key="link">注销用户</a>
+      ],
+    },
+  ];
   return (
     <PageContainer>
-      <Card>
-        <Space>
-          <Button type="primary" icon={<PlusOutlined />}>
-            添加用户
-          </Button>
+      <ProTable<UserTableListItem>
+        dataSource={userTableListDataSource}
+        rowKey="key"
+        pagination={{
+          showQuickJumper: true,
+        }}
+        columns={columns}
+        dateFormatter="string"
+        headerTitle="用户列表"
+        toolBarRender={() => [
+        <Button key="out">导出数据</Button>,
+        <Button key="out" icon={<PlusOutlined />}>新建用户</Button>,
 
-          <Tooltip title="刷新">
-            <Button icon={<SearchOutlined />} />
-          </Tooltip>
-
-          <Tooltip title="密度">
-            <Button icon={<ColumnHeightOutlined />}></Button>
-          </Tooltip>
-
-          <Tooltip title="列设置">
-            <Button icon={<SettingOutlined />}></Button>
-          </Tooltip>
-        </Space>
-
-        <Table dataSource={dataSource}>
-          <Column title="Id" dataIndex="id" key="id" />
-          <Column title="用户名" dataIndex="username" key="username" />
-          <Column title="密码" dataIndex="password" key="password" />
-          <Column
-            title="用户角色"
-            dataIndex="role"
-            key="role"
-            render={(role: string, record: any) => (
-              <>
-                {record.role === '管理员' ? (
-                  <Tag color="volcano" key="role">
-                    {role}
-                  </Tag>
-                ) : record.role === '公益企业' ? (
-                  <Tag color="blue" key="role">
-                    {role}
-                  </Tag>
-                ) : (
-                  <Tag color="green" key="role">
-                    {role}
-                  </Tag>
-                )}
-              </>
-            )}
+      ]}
+      />
+      <Drawer
+        width={500}
+        visible={showDetail}
+        onClose={() => {
+          setCurrentRow(undefined);
+          setShowDetail(false);
+        }}
+        closable={true}
+      >
+        {currentRow?.username && (
+          <ProDescriptions<UserTableListItem>
+            column={1}
+            title={currentRow?.username + '的个人信息'}
+            request={async () => ({
+              data: currentRow || {},
+            })}
+            params={{
+              id: currentRow?.id,
+            }}
+            columns={columns as ProDescriptionsItemProps<UserTableListItem>[]}
           />
-          <Column title="注册时间" dataIndex="createTime" key="createTime" />
-
-          <Column
-            title="操作"
-            key="action"
-            render={(_: any) => (
-              <Space size="middle">
-                <a>重置密码</a>
-                <a>注销用户</a>
-              </Space>
-            )}
-          />
-        </Table>
-      </Card>
+        )}
+      </Drawer>
     </PageContainer>
   );
 };
 
-export default UserManagement;
+export default ApplyTableList;
