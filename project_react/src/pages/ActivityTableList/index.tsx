877e47fbabdc6aa4401a7c3128e6b4521ab553aa
17612937572 <2480243521@qq.com>
2023-01-16 14:07:41 +0800

    minor

--- project_react/src/pages/ActivityTableList/index.tsx
+++ project_react/src/pages/ActivityTableList/index.tsx
@@ -10,7 +10,6 @@ import {
   ProFormTextArea,
   ProTable,
 } from '@ant-design/pro-components';
-import { FormattedMessage, useIntl } from '@umijs/max';
 import { Button, Drawer, message } from 'antd';
 import React, { useRef, useState } from 'react';
 import type { FormValueType } from './components/UpdateForm';
@@ -119,11 +118,6 @@ const handleRemove = async (selectedRows: API.RuleListItem[]) => {
 
 const ActivityTableList: React.FC = () => {
   /**
-   * @en-US Pop-up window of new window
-   * @zh-CN 新建窗口的弹窗
-   *  */
-  const [createModalVisible, handleModalVisible] = useState<boolean>(false);
-  /**
    * @en-US The pop-up window of the distribution update window
    * @zh-CN 分布更新窗口的弹窗
    * */
@@ -140,16 +134,10 @@ const ActivityTableList: React.FC = () => {
    * @en-US International configuration
    * @zh-CN 国际化配置
    * */
-  const intl = useIntl();
 
   const columns: ProColumns<API.RuleListItem>[] = [
     {
-      title: (
-        <FormattedMessage
-          id="pages.searchActivityTable.updateForm.ruleName.nameLabel"
-          defaultMessage="活动名称"
-        />
-      ),
+      title: '活动名称',
       dataIndex: 'name',
       tip: 'The rule name is the unique key',
       render: (dom, entity) => {
@@ -166,74 +154,49 @@ const ActivityTableList: React.FC = () => {
       },
     },
     {
-      title: (
-        <FormattedMessage id="pages.searchActivityTable.titleDesc" defaultMessage="活动描述" />
-      ),
+      title: '活动描述',
       dataIndex: 'desc',
       valueType: 'textarea',
     },
     {
-      title: <FormattedMessage id="pages.searchActivityTable.address" defaultMessage="活动地点" />,
+      title: '活动地点',
       dataIndex: 'address',
       // sorter: true,
       hideInForm: true,
     },
     {
-      title: (
-        <FormattedMessage id="pages.searchActivityTable.startDate" defaultMessage="开始日期" />
-      ),
+      title: '开始日期',
       dataIndex: 'startDate',
       sorter: true,
       hideInForm: true,
     },
     {
-      title: (
-        <FormattedMessage id="pages.searchActivityTable.startTime" defaultMessage="开始时间" />
-      ),
+      title: '开始时间',
       dataIndex: 'startTime',
       sorter: true,
       hideInForm: true,
     },
     {
-      title: (
-        <FormattedMessage
-          id="pages.searchActivityTable.requirements"
-          defaultMessage="志愿者素养要求"
-        />
-      ),
+      title: '志愿者素养要求',
       dataIndex: 'requirements',
       hideInForm: true,
     },
     {
-      title: (
-        <FormattedMessage id="pages.searchActivityTable.needPersonNum" defaultMessage="需要人数" />
-      ),
+      title: '需要人数',
       dataIndex: 'needPersonNum',
     },
     {
-      title: (
-        <FormattedMessage
-          id="pages.searchActivityTable.applyPersonNum"
-          defaultMessage="已报名人数"
-        />
-      ),
+      title: '已报名人数',
       dataIndex: 'applyPersonNum',
     },
     {
-      title: (
-        <FormattedMessage
-          id="pages.searchActivityTable.passPersonNum"
-          defaultMessage="审核通过人数"
-        />
-      ),
+      title: '审核通过人数',
       dataIndex: 'passPersonNum',
     },
     {
-      title: (
-        <FormattedMessage id="pages.searchActivityTable.titleOption" defaultMessage="Operating" />
-      ),
+      title: '操作',
       dataIndex: 'option',
-      width: 290,
+      width: 180,
       valueType: 'option',
       render: (_, record) => [
         <a
@@ -243,7 +206,7 @@ const ActivityTableList: React.FC = () => {
             setCurrentRow(record);
           }}
         >
-          <FormattedMessage id="pages.searchActivityTable.apply" defaultMessage="点击报名" />
+          报名
         </a>,
         <a
           key="details"
@@ -252,7 +215,7 @@ const ActivityTableList: React.FC = () => {
             setShowDetail(true);
           }}
         >
-          <FormattedMessage id="pages.searchActivityTable.details" defaultMessage="查看详情" />
+          详情
         </a>,
         <a
           key="edit"
@@ -261,11 +224,9 @@ const ActivityTableList: React.FC = () => {
             setCurrentRow(record);
           }}
         >
-          <FormattedMessage id="pages.searchActivityTable.edit" defaultMessage="编辑活动" />
+          编辑
         </a>,
-        <a>
-          <FormattedMessage id="pages.searchActivityTable.delete" defaultMessage="删除活动" />
-        </a>,
+        <a>删除</a>,
       ],
     },
   ];
@@ -273,10 +234,7 @@ const ActivityTableList: React.FC = () => {
   return (
     <PageContainer>
       <ProTable<API.RuleListItem, API.PageParams>
-        headerTitle={intl.formatMessage({
-          id: 'pages.searchActivityTable.title',
-          defaultMessage: 'Enquiry form',
-        })}
+        headerTitle="活动列表"
         actionRef={actionRef}
         rowKey="key"
         search={{
@@ -287,10 +245,11 @@ const ActivityTableList: React.FC = () => {
             type="primary"
             key="primary"
             onClick={() => {
-              handleModalVisible(true);
+              handleUpdateModalVisible(true);
             }}
           >
-            <PlusOutlined /> <FormattedMessage id="pages.searchTable.new" defaultMessage="New" />
+            <PlusOutlined />
+            新建活动
           </Button>,
         ]}
         request={rule}
@@ -305,18 +264,7 @@ const ActivityTableList: React.FC = () => {
         <FooterToolbar
           extra={
             <div>
-              <FormattedMessage id="pages.searchTable.chosen" defaultMessage="Chosen" />{' '}
-              <a style={{ fontWeight: 600 }}>{selectedRowsState.length}</a>{' '}
-              <FormattedMessage id="pages.searchTable.item" defaultMessage="项" />
-              &nbsp;&nbsp;
-              <span>
-                <FormattedMessage
-                  id="pages.searchTable.totalServiceCalls"
-                  defaultMessage="Total number of service calls"
-                />{' '}
-                {/* {selectedRowsState.reduce((pre, item) => pre + item.callNo!, 0)}{' '} */}
-                {/* <FormattedMessage id="pages.searchTable.tenThousand" defaultMessage="万" /> */}
-              </span>
+              已选择 <a style={{ fontWeight: 600 }}>{selectedRowsState.length}</a> 项 &nbsp;&nbsp;
             </div>
           }
         >
@@ -327,54 +275,11 @@ const ActivityTableList: React.FC = () => {
               actionRef.current?.reloadAndRest?.();
             }}
           >
-            <FormattedMessage
-              id="pages.searchTable.batchDeletion"
-              defaultMessage="Batch deletion"
-            />
+            批量删除
           </Button>
-          <Button type="primary">
-            <FormattedMessage
-              id="pages.searchTable.batchApproval"
-              defaultMessage="Batch approval"
-            />
-          </Button>
+          <Button type="primary">批量导出</Button>
         </FooterToolbar>
       )}
-      <ModalForm
-        title={intl.formatMessage({
-          id: 'pages.searchActivityTable.createForm.newActivity',
-          defaultMessage: '新建活动',
-        })}
-        width="400px"
-        visible={createModalVisible}
-        onVisibleChange={handleModalVisible}
-        onFinish={async (value) => {
-          const success = await handleAdd(value as API.RuleListItem);
-          if (success) {
-            handleModalVisible(false);
-            if (actionRef.current) {
-              actionRef.current.reload();
-            }
-          }
-        }}
-      >
-        <ProFormText
-          rules={[
-            {
-              required: true,
-              message: (
-                <FormattedMessage
-                  id="pages.searchTable.ruleName"
-                  defaultMessage="Rule name is required"
-                />
-              ),
-            },
-          ]}
-          width="md"
-          name="name"
-        />
-        <ProFormTextArea width="md" name="desc" />
-      </ModalForm>
       <UpdateForm
         onSubmit={async (value) => {
           const success = await handleUpdate(value);
