345e7859c485035957227b2aa2a013752a603150
17612937572 <2480243521@qq.com>
2023-01-31 16:01:27 +0800

    update: date time format

--- project-drf/users/serializers.py
+++ project-drf/users/serializers.py
@@ -10,8 +10,8 @@ class UserSerializer(serializers.ModelSerializer):
 		model = User
 		fields = ('id', 'username', 'groups', 'first_name', 'last_name', 'email', 'last_login', 'date_joined', 'password')
 		
-	last_login = serializers.DateTimeField(label='上次登录时间', required=False, format='%Y-%d-%m %H:%M:%S')
-	date_joined = serializers.DateTimeField(label='注册时间', required=False, format='%Y-%d-%m %H:%M:%S')
+	last_login = serializers.DateTimeField(label='上次登录时间', required=False, format='%Y-%m-%d %H:%M:%S')
+	date_joined = serializers.DateTimeField(label='注册时间', required=False, format='%Y-%m-%d %H:%M:%S')
 	groups = serializers.SlugRelatedField(many=True, queryset=Group.objects.all(), slug_field="name")
 	password = serializers.CharField(label='密码', write_only=True)
 
